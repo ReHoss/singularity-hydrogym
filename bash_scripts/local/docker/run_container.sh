7a78c8309d367010f8550670c767b00ed03916c8
Rémy Hosseinkhan <hosseinkhan@lisn.fr>
2024-09-11 13:58:31 +0200

    Update the running file for container
    Since the image does not pip install -e anymore
    , it is necessary to mount the source code inside the container.

--- bash_scripts/local/docker/run_container.sh
+++ bash_scripts/local/docker/run_container.sh
@@ -14,14 +14,11 @@ docker run \
   --rm \
   --mount type=bind,source="$PATH_CONTENT_ROOT"/data,target="$PATH_CONTAINER_CONTENT_ROOT"/data \
   --mount type=bind,source="$PATH_CONTENT_ROOT"/configs,target="$PATH_CONTAINER_CONTENT_ROOT"/configs \
+  --mount type=bind,source="$PATH_CONTENT_ROOT"/singularity_hydrogym,target="$PATH_CONTAINER_CONTENT_ROOT"/singularity_hydrogym \
   "$NAME_CONTAINER"
 
 
-# Legacy: It used to be necessary to mount the source code inside the container.
-#  --mount type=bind,source="$PATH_CONTENT_ROOT"/src,target="$PATH_CONTAINER_CONTENT_ROOT"/src \
-
-
-  # --read-only: Mount the container's root filesystem as read-only to check compatibility with Singularity
-  # Indeed, Singularity containers are read-only by default
-  # Unfortunately, firedrake writes at least in /home/firedrake/.cache/pytools in the container,
-  # so we cannot use --read-only. Consequently, the decision is to make the Singularity container writable
+# --read-only: Mount the container's root filesystem as read-only to check compatibility with Singularity
+# Indeed, Singularity containers are read-only by default
+# Unfortunately, firedrake writes at least in /home/firedrake/.cache/pytools in the container,
+# so we cannot use --read-only. Consequently, the decision is to make the Singularity container writable
